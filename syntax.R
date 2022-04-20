f5e712a460d446817b7da8ea7409e3facfc28252
Angel A. García O'Diana <79344751+AngelGarciaODiana@users.noreply.github.com>
2022-04-19 23:47:11 -0500

    Add files via upload

---
+++ syntax.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+
+df_0 <- read_csv("Automotive_1.csv", quote = "\"",
+                               col_types = cols(price = col_number(), 
+                                                yearofregistration = col_number(), 
+                                                powerps = col_number(), kilometer = col_number(), 
+                                                monthofregistration = col_number(), 
+                                                postalcode = col_number()))
+
+problems <- problems()
+
+unique <- data.frame(unique(problems$row))
+
+
+df <- read.table(text=readLines(file.choose(), warn = FALSE), as.is = TRUE)
+
+df_1 <- data.frame(str_split_fixed(df$V1, ",", 20))
+
+df_2 <- gsub(pattern = "?", replacement = ",", df_1$X2, fixed = T)
+df_2 <- do.call(rbind.data.frame, strsplit(df_2, split = ",", 2))
+
+
+filter <- df_1 %>%
+  filter(X3 != "privat") %>%
+  filter(X4 != "", X4 != "Angebot")
+
+
+
+da <- read.table( 
+  text = readLines(file.choose(), warn = FALSE), 
+  header = TRUE,  
+  sep = ",",
+  quote = "\"",
+  na.strings = "NA")
+
+da <- Automotive_1
+
+summary(da)
+
+sum(is.na(da$abtest))
+
+filter(is.na(da$price))
+
+ifelse(A$box == 6 & A$document == 75, "big", "other")
+
+
+
+
