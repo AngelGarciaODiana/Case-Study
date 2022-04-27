1c65f77c1ab86f57c428ca64473278dd808b1c77
Angel A. García O'Diana <79344751+AngelGarciaODiana@users.noreply.github.com>
2022-04-27 17:24:01 -0500

    Update syntax.R

--- syntax.R
+++ syntax.R
@@ -1,3 +1,42 @@
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(Hmisc)
+
+
+# Import data
+data<- read_csv("C:/Users/angel/Desktop/Case Study/Automotive_1.csv",quote = "\"",
+                col_types = cols(
+                  .default = col_character()))
+
+# Observations with wrong columns
+error_cases <- data[grep("Angebot", data$seller),1:(ncol(data)-1)]
+
+colnames(error_cases)[3:ncol(error_cases)] <- colnames(data)[4:ncol(data)]
+error_cases$seller <- "privat"
+
+# Corrected data frame
+data <- rbind.data.frame(data[-(grep("Angebot", data$seller)),], error_cases)
+
+char_vars <- c("name", "seller", "offertype",	"abtest", "vehicletype", "gearbox", "model", "fueltype", "brand", 
+               "notrepaireddamage", "nrofpictures", "postalcode")
+
+num_vars <- c("price", "yearofregistration","powerps","kilometer","monthofregistration")
+data[num_vars] <- lapply(data[num_vars], function(x) as.numeric(as.character(x)))
+
+date_vars <- c("datecrawled", "datecreated","lastseen")
+data[date_vars] <- lapply(data[date_vars], function(x) as.Date(as.character(x)))
+
+
+#####Descriptive statistics
+describe(data)
+
+
+
+
+
+
+
 library(tidyverse)
 
 df_0 <- read_csv("C:/Users/angel/Desktop/Case Study/Automotive_1.csv", quote = "\"",
